e5addc0588ef82b3b39d497f6589f6b99e2bc3d0
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2025-02-08 10:31:23 -0500

    Update direccion IP del servidor retrofitClient.kt

--- app/src/main/java/com/example/tarjetahospitalespinar/RetrofitClient.kt
+++ app/src/main/java/com/example/tarjetahospitalespinar/RetrofitClient.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.69.175:3000/"
+    //private const val BASE_URL = "http://192.168.69.175:3000/"
+    private const val BASE_URL = "http://181.176.21.151:1021/"
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
